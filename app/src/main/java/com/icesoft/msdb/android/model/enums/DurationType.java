7528971d4ec7ca1f198addd0a659946a95e896d5
Ivan Clavijos <ivan.clavijos@sngular.com>
2022-08-29 22:13:26 -0400

    Fixed handling of response values

--- app/src/main/java/com/icesoft/msdb/android/model/enums/DurationType.java
+++ app/src/main/java/com/icesoft/msdb/android/model/enums/DurationType.java
@@ -1,9 +1,18 @@
 package com.icesoft.msdb.android.model.enums;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+
+import java.util.Locale;
+
 public enum DurationType {
 	MINUTES,
 	HOURS,
 	KMS,
 	MILES,
 	LAPS;
+
+	@JsonCreator
+	public static DurationType fromString(String value) {
+		return DurationType.valueOf(value.toUpperCase(Locale.ROOT));
+	}
 }
--- app/src/main/java/com/icesoft/msdb/android/model/enums/SessionType.java
+++ app/src/main/java/com/icesoft/msdb/android/model/enums/SessionType.java
@@ -1,9 +1,21 @@
 package com.icesoft.msdb.android.model.enums;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+
+import java.util.Locale;
+
 public enum SessionType {
 	PRACTICE,
 	QUALIFYING,
 	RACE,
 	QUALIFYING_RACE,
     STAGE;
+
+	@JsonCreator
+	public static SessionType fromString(String value) {
+		if (value.equals("qualifyingRace")) {
+			return QUALIFYING_RACE;
+		}
+		return SessionType.valueOf(value.toUpperCase(Locale.ROOT));
+	}
 }
